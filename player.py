66257d1c1401d51d638e29518d97c96924097cb8
Milen Orfeev <milen.orfeev@abv.bg>
2018-11-11 03:16:04 +0000

    Modified identation in player

--- player.py
+++ player.py
@@ -1,22 +1,22 @@
 class Player:
-    def __init__(self):
-        self.health = 100
-        self.money = 1000
+  def __init__(self):
+    self.health = 100
+    self.money = 1000
 
-    def increaseHealth(self, amount):
-     self.health += amount
+  def increaseHealth(self, amount):
+   self.health += amount
 
-    def decreaseHealth(self, amount):
-     self.health -= amount
+  def decreaseHealth(self, amount):
+   self.health -= amount
 
-    def increaseMoney(self, amount):
-     self.money += amount
+  def increaseMoney(self, amount):
+   self.money += amount
 
-    def decreaseMoney(self, amount):
-     self.money -= amount
+  def decreaseMoney(self, amount):
+   self.money -= amount
 
-    def getHealth(self):
-     return self.health
+  def getHealth(self):
+   return self.health
 
-    def getMoney(self):
-     return self.money
+  def getMoney(self):
+   return self.money
